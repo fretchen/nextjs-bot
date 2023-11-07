c150bb01e0067da3b94e1c1a99337ecd3a1b61ff
fretchen <fred.jendrzejewski@gmail.com>
2023-11-07 11:42:00 +0100

    Connect the input with the text above.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import React, { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,19 +10,59 @@ const inter = Inter({ subsets: ["latin"] });
 function LeftRegion() {
   return (
     <div className={styles.left}>
-      I am on the left.
-      {/* Content for the left region goes here */}
+      <p>
+        I am on the left. <br />
+        One day you might be able to see uploaded documents up here.
+      </p>
+    </div>
+  );
+}
+
+// Define the RightText component
+function RightText({ text }) {
+  return (
+    <div className={styles.right}>
+      <p>{text}</p>
+    </div>
+  );
+}
+
+// Define the InputWithButton component
+function InputWithButton({ setText }) {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    setText(inputValue);
+    setInputValue("");
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        className={styles.inputField}
+      />
+      <button onClick={handleButtonClick} className={styles.sendButton}>
+        Send
+      </button>
     </div>
   );
 }
 
 // Define the RightRegion component
 function RightRegion() {
+  const [text, setText] = useState("I am on the right and get text.");
+
   return (
     <div className={styles.right}>
-      <p>I am on the right.</p>
-      <input type="text" className={styles.inputField} />
-      <button className={styles.sendButton}>Send</button>
+      <RightText text={text} />
+      <InputWithButton setText={setText} />
     </div>
   );
 }
