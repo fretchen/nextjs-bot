1b2a1b9a8feab403f1afbc81a0694052d914699a
fretchen <fred.jendrzejewski@gmail.com>
2023-11-10 08:18:27 +0100

    Something is wrong with the API for the moment.
    Not sure what is going on...

--- src/components/chat-region.js
+++ src/components/chat-region.js
@@ -1,6 +1,9 @@
+"use client";
+
 import React, { useState } from "react";
 import styles from "./chat-region.module.css";
 
+import { useChat } from "ai/react";
 function MessageBox({ message }) {
   return <div className={styles.messageBox}>{message}</div>;
 }
@@ -65,20 +68,26 @@ function InputWithButton({ addMessage, resetMessages }) {
 
 // Define the ChatRegion component
 function ChatRegion() {
-  const [messages, setMessages] = useState(["I am on the right and get text."]);
-
-  const addMessage = (message) => {
-    setMessages((prevMessages) => [...prevMessages, message]);
-  };
-
-  const resetMessages = () => {
-    setMessages([]);
-  };
-
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
-    <div className={styles.right}>
-      <RightText messages={messages} />
-      <InputWithButton addMessage={addMessage} resetMessages={resetMessages} />
+    <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
+      {messages.length > 0
+        ? messages.map((m) => (
+            <div key={m.id} className="whitespace-pre-wrap mb-4">
+              <b>{m.role === "user" ? "User: " : "AI: "}</b>
+              {m.content}
+            </div>
+          ))
+        : null}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2"
+          value={input}
+          placeholder="Say something..."
+          onChange={handleInputChange}
+        />
+      </form>
     </div>
   );
 }
