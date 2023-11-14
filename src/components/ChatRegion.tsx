a9b776616a51dfa5ae8bab79181117373fc139a2
fretchen <fred.jendrzejewski@gmail.com>
2023-11-14 15:57:46 +0100

    First working prototype with Ollama

--- src/components/ChatRegion.tsx
+++ src/components/ChatRegion.tsx
@@ -1,100 +1,18 @@
 "use client";
 
 import React, { useState } from "react";
-import styles from "./chat-region.module.css";
 
 import { useChat } from "ai/react";
 
-type MessageBoxProps = {
-  message: string;
-};
-
-function MessageBox({ message }: MessageBoxProps) {
-  return <div className={styles.messageBox}>{message}</div>;
-}
-
-function AnswerBox() {
-  const letters = "abcdefghijklmnopqrstuvwxyz";
-  let randomString = "";
-  for (let i = 0; i < 20; i++) {
-    randomString += letters[Math.floor(Math.random() * letters.length)];
-  }
-
-  return <div className={styles.answerBox}>{randomString}</div>;
-}
-
-function RightText({messages}: { messages : string[] }) {
-  return (
-    <div className={styles.right}>
-      {messages.map((message, index) => (
-        <React.Fragment key={index}>
-          <MessageBox message={message} />
-          <AnswerBox />
-        </React.Fragment>
-      ))}
-    </div>
-  );
-}
-
-// Define the InputWithButton component
-
-type InputWithButtonProps = {
-  addMessage: (message: string) => void;
-  resetMessages: () => void;
-};
-
-function InputWithButton({ addMessage, resetMessages }: InputWithButtonProps) {
-  const [inputValue, setInputValue] = useState("");
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
-  };
-
-  const handleButtonClick = () => {
-    addMessage(inputValue);
-    setInputValue("");
-  };
-
-  const handleResetClick = () => {
-    resetMessages();
-  };
-
-  return (
-    <div>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        className={styles.inputField}
-      />
-      <button onClick={handleButtonClick} className={styles.sendButton}>
-        Send
-      </button>
-      <button onClick={handleResetClick} className={styles.resetButton}>
-        Reset
-      </button>
-    </div>
-  );
-}
-
-// Define the ChatRegion component
 function ChatRegion() {
-  const [messages, setMessages] = useState(["I am on the right and get text."]);
-
-  const addMessage = (message:string):void => {
-    setMessages((prevMessages: string[]) => [...prevMessages, message]);
-  };
-
-  const resetMessages = () => {
-    setMessages([]);
-  };
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   return (
     <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
       {messages.length > 0
-        ? messages.map((m) => (
+        ? messages.map(m => (
             <div key={m.id} className="whitespace-pre-wrap mb-4">
-              <b>{m.role === "user" ? "User: " : "AI: "}</b>
+              <b>{m.role === 'user' ? 'User: ' : 'AI: '}</b>
               {m.content}
             </div>
           ))
--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -5,7 +5,7 @@ import { BytesOutputParser } from "langchain/schema/output_parser";
 
 export const runtime = "edge";
 
-export async function POST(req) {
+export default async function POST(req: Request) {
   const { messages } = await req.json();
 
   const model = new ChatOllama({
@@ -18,7 +18,7 @@ export async function POST(req) {
   const stream = await model
     .pipe(parser)
     .stream(
-      messages.map((m) =>
+      (messages as Message[]).map((m) =>
         m.role == "user"
           ? new HumanMessage(m.content)
           : new AIMessage(m.content)
